2da756b11da68dbd6ee7e2282e159309692c4daf
Your Name <lucaslunzmann@gmail.com>
2023-01-28 15:46:13 -0600

    A lot of tailwind

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,21 @@ export default function Home() {
       <Head>
         <title>Lucas's Site</title>
       </Head>
-      <section className={utilStyles.headingMd}>
       <br></br>
-        <p>Hey! My name is <b>Lucas Lunzmann</b> and I am a Business Development Associate at <a href="https://datafeel.com/">DataFeel®</a> in Omaha, NE.</p>
-        <br></br>
-        <p>This is my very first Next.js web app and I seriously could not be more excited! This basic little page is such a meanigful win for me. This is the outcome of months and months of learning, failing, and creating a daily habit of coding.</p>
-        <br></br>
-        <p>I feel like the floodgates have opened - we have crossed off a major barrier of entry. It's just a matter of time before we start shipping. 🚢</p>
-        <br></br>
-        <h2 className= "text-3xl font-bold">
-          Skills</h2>
-        <hr></hr>
-        <p>
-          <table>
+      <section className="font-sans text-lg">
+        <p>Hey! My name is <b>Lucas Lunzmann</b> and I am a Business Development Associate at <u><a class="hover:text-blue-600" href="https://datafeel.com/">DataFeel®</a></u> in Omaha, NE.
+        <br></br><br></br>
+        This is my very first Next.js web app and I seriously could not be more excited! This basic little page is such a meanigful win for me. This is the outcome of months and months of learning, failing, and creating a daily habit of coding.
+        <br></br><br></br>
+        I feel like the floodgates have opened - we have crossed off a major barrier of entry. It's just a matter of time before we start shipping. 🚢
+        </p>
+      </section>
+        <br></br><br></br>
+        <h3 class="font-sans uppercase text-lg text-center">Always Learning</h3>
+        <h2 className= "text-center text-5xl text-blue-600 font-bold">
+          Value-Add Skills</h2>
+        <p> <br></br>
+          <table class="m-auto">
             <tr><td>1.</td><td>Notion</td></tr>
             <tr><td>2.</td><td>Canva</td></tr>
             <tr><td>3.</td><td>WordPress</td></tr>
@@ -28,8 +30,6 @@ export default function Home() {
             <tr><td>5.</td><td>Copywriting</td></tr>
           </table>
         </p>
-        
-      </section>
     </Layout>
   );
 }
